529d74d741ee367923181e9fa2d5aaae7abe104c
Vincent <oa5136@gmail.com>
2025-03-18 16:27:21 +0800

    Remove unnecessary components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
-import Loading from '@/components/Loading'
 import FrontLayout from '@/layouts/FrontLayout';
 import Weather from '@/components/Weather'
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="/" element={<FrontLayout />}>
-            <Route
-              index
-              element={
-                <Weather>
-                  <Weather.Search />
-                  <Weather.Weather_Current />
-                  <Weather.Weather_forecast />
-                </Weather>
-              }
-            />
-          </Route>
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<FrontLayout />}>
+          <Route
+            index
+            element={
+              <Weather>
+                <Weather.Search />
+                <Weather.Weather_Current />
+                <Weather.Weather_forecast />
+              </Weather>
+            }
+          />
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
